5de3519a367c6b16a61b85bf1a69b5b2794bcc39
Ubuntu <ubuntu@ip-172-31-90-165.ec2.internal>
2024-12-30 18:40:09 +0000

    Fixed DELETE button

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,17 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[show edit update destroy]
+  before_action :set_book, only: %i[ show edit update destroy ]
 
-  # GET /books
   def index
     @books = Book.all
   end
 
-  # GET /books/1
   def show
   end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
-  def edit
-  end
-
-  # POST /books
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -29,7 +21,9 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1
+  def edit
+  end
+
   def update
     if @book.update(book_params)
       redirect_to @book, notice: 'Book was successfully updated.'
@@ -38,21 +32,21 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1
   def destroy
-    @book.destroy
-    redirect_to books_url, notice: 'Book was successfully deleted.'
+    @book = Book.find(params[:id])
+    @book.destroy!
+    redirect_to books_path, notice: 'Book was successfully deleted (force).'
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_book
     @book = Book.find_by(id: params[:id])
-    redirect_to books_path, alert: 'Book not found.' if @book.nil?
+    unless @book
+      redirect_to books_path, alert: "Book not found"
+    end
   end
 
-  # Only allow a list of trusted parameters through.
   def book_params
     params.require(:book).permit(:title, :author, :description)
   end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// Entry point for the build script in your package.json
+import { Turbo } from "@hotwired/turbo-rails";
+import Rails from "@rails/ujs";
+Rails.start();
+Turbo.session.drive = true;
+
+// Controllers (Stimulus)
+import "./controllers";
+
+// Bootstrap (Optional)
+import * as bootstrap from "bootstrap";
+
+// Prevent forms from submitting twice by disabling buttons
+document.addEventListener("turbo:submit-start", function (event) {
+  const submitButton = event.target.querySelector("input[type='submit'], button[type='submit']");
+  if (submitButton) {
+    submitButton.disabled = true;
+  }
+});
